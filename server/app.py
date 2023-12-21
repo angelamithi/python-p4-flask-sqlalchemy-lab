06d4e0a159403bec8a02522bb94f1338e8b1c4dc
+ <+>
2023-12-21 16:42:04 +0300

    first commit

--- server/app.py
+++ server/app.py
@@ -19,15 +19,37 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal=Animal.query.filter(Animal.id==id).first()
+    response_body=f'''
+    <ul>{animal.name}</ul>
+    <ul>{animal.species}</ul>
+    <ul>{animal.zookeeper.name}</ul>
+    <ul>{animal.enclosure.environment}</ul>
+'''
+    response=make_response(response_body,200)
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper=Zookeeper.query.filter(Zookeeper.id==id).first()
+    response_body=f'''
+<ul>{zookeeper.name}</ul>
+<ul>{zookeeper.birthday}</ul>
+<ul>{zookeeper.animals.name}</ul>
 
+'''
+    response=make_response(response_body,200)
+    return response
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure=Enclosure.query.filter(Enclosure.id==id).first()
+    response_body=f'''
+<ul>{enclosure.environment}</ul>
+<ul>{enclosure.open_to_visitors}
+<ul>{enclosure.animals}</ul>
+'''
+    response=make_response(response_body,200)
+    return response
 
 
 if __name__ == '__main__':
