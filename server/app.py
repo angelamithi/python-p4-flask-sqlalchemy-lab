9fc39669803245c84e900afa1fb41067e4df311a
+ <+>
2023-12-21 19:52:38 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -21,20 +21,23 @@ def home():
 def animal_by_id(id):
     animal=Animal.query.filter(Animal.id==id).first()
     response_body=f'''
-    <ul>{animal.name}</ul>
-    <ul>{animal.species}</ul>
-    <ul>{animal.zookeeper.name}</ul>
-    <ul>{animal.enclosure.environment}</ul>
+    <ul>Name:{animal.name}</ul>
+    <ul>Species:{animal.species}</ul>
+    <ul>Zookeeper:{animal.zookeeper.name}</ul>
+    <ul>Enclosure:{animal.enclosure.environment}</ul>
+    
 '''
     response=make_response(response_body,200)
+    print(response.data.decode())
+
     return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
     zookeeper=Zookeeper.query.filter(Zookeeper.id==id).first()
     response_body=f'''
-<ul>{zookeeper.name}</ul>
-<ul>{zookeeper.birthday}</ul>
+<ul>Name:{zookeeper.name}</ul>
+<ul>Birthday:{zookeeper.birthday}</ul>
 
 '''
     animals=[animal for animal in zookeeper.animals]
@@ -42,15 +45,15 @@ def zookeeper_by_id(id):
         response_body += f'<h2>Has not been assigned any animals at this time.</h2>'
     else:
         for animal in animals:
-             response_body +=f'<h2>Takes care of {animal.name}</h2>'
+             response_body +=f'<ul>Animal:{animal.name}</ul>'
     response=make_response(response_body,200)
     return response
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
     enclosure=Enclosure.query.filter(Enclosure.id==id).first()
     response_body=f'''
-<ul>{enclosure.environment}</ul>
-<ul>{enclosure.open_to_visitors}
+<ul>Environment:{enclosure.environment}</ul>
+<ul>Open to Visitors.{enclosure.open_to_visitors}</ul>
 
 '''
     animals=[animal for animal in enclosure.animals]
@@ -58,7 +61,7 @@ def enclosure_by_id(id):
         response_body=f'<h2>No animals in the {enclosure.environment} closure</h2>'
     else:
         for animal in animals:
-            response_body+=f'<ul>{animal.name}</ul>'
+            response_body+=f'<ul>Animal:{animal.name}</ul>'
     response=make_response(response_body,200)
     return response
 
